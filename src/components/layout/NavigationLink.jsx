55f2482e3d4c9f9d35567abef6e4d84c3baab61b
Dinesh Pawar <dsp70458@gmail.com>
2025-05-05 16:32:19 +0530

    fix: Correct formatting and spacing in NavigationLink component; improve readability of mail and pending count display

--- src/components/layout/NavigationLink.jsx
+++ src/components/layout/NavigationLink.jsx
@@ -9,7 +9,7 @@ const NavigationLink = ({
   mailMsg = false,
   pendingApproval = false,
   pendingCount,
-  mailCount
+  mailCount,
 }) => {
   return (
     <NavLink
@@ -33,25 +33,59 @@ const NavigationLink = ({
         <span className="absolute bottom-[145px] left-[38px] w-[11px] h-[10px] rounded-full border-2 border-[#FAFBFF] bg-[#EA4646]" />
       )}
 
-
       {/* email */}
       {mailCount > 0 && (
         <span className="absolute bottom-[331px] left-[35px] min-w-[16px] h-[16px] px-[5px] text-[10px] flex items-center justify-center text-white bg-red-600 rounded-full font-semibold leading-none">
-          {mailCount > 120 ? '120+' : mailCount}
+          {mailCount > 120 ? "120+" : mailCount}
         </span>
       )}
 
-
-{pendingCount > 0 && (
+      {pendingCount > 0 && (
         <span className="absolute bottom-[288px] left-[35px] min-w-[16px] h-[16px] px-[5px] text-[10px] flex items-center justify-center text-white bg-red-600 rounded-full font-semibold leading-none">
-          {pendingCount > 120 ? '120+' : pendingCount}
+          {pendingCount > 120 ? "120+" : pendingCount}
         </span>
       )}
-
-
-            
     </NavLink>
   );
 };
 
 export default NavigationLink;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// *********** FUTURE: Real-time msg NUMBERS via WS/SSE )   (EX.120)      **************
+
+
+
+
+// const [inboxCount, setInboxCount] = useState(0)
+// const [pendingCount, setPendingCount] = useState(0)
+
+// useEffect(() => {
+//   const socket = new WebSocket("wss://yourserver.com/notifications")
+
+//   socket.onmessage = (event) => {
+//     const data = JSON.parse(event.data)
+//     setInboxCount(data.inboxCount)
+//     setPendingCount(data.pendingCount)
+//   }
+
+//   return () => socket.close()
+// }, [])
